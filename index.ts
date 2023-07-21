34c7569421be68084ad9a5c2838f1dfb1ab93e7a
Zahin Mohammad <zahinmohammad@bitgo.com>
2023-07-21 16:49:32 -0400

    feat: allow recovering tokens with eth tss

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import {BitGoAPI} from './BitGoJS/modules/sdk-api';
 import {Eth, RecoverOptions} from "./BitGoJS/modules/sdk-coin-eth";
 import recoveryOptions from './keycard.json';
-import {AliasEnvironments} from "./BitGoJS/modules/sdk-core";
+import {AliasEnvironments, Wallet} from "./BitGoJS/modules/sdk-core";
+
+const arg = process.argv.slice(2).length? process.argv.slice(2)[0] : '';
+assert(arg, 'expected recover or recoverToken <address> as arguments');
+console.log(arg);
 
 const sdk = new BitGoAPI({env: AliasEnvironments.production});
 
@@ -14,7 +18,19 @@ async function recover(): Promise<void> {
         isTss: true,
         // not needed for tss
         walletContractAddress: "",
-    });
+    }, false);
+    console.log(JSON.stringify(res, null, 2));
+}
+
+async function recoverToken(): Promise<void> {
+    const tokenContractAddress = process.argv.slice(2)[1];
+    assert(tokenContractAddress, 'missing token constract address');
+    const options: Omit<RecoverOptions, 'isTss' | 'walletContractAddress'> = recoveryOptions;
+    const res = await baseCoin.recoverTokenTss({
+        ...options,
+        tokenContractAddress,
+        isTss: true,
+    }, false);
     console.log(JSON.stringify(res, null, 2));
 }
 
@@ -30,5 +46,11 @@ async function time(fn: () => Promise<void>, name: string): Promise<number> {
     return (end - start) / 1000;
 }
 
-time(recover, 'recover').catch((e) => console.error(e));
+if (arg === 'recover') {
+    time(recover, 'recover').catch((e) => console.error(e));
+} else if (arg === 'recoverToken') {
+    time(recoverToken, 'recoverToken').catch((e) => console.error(e));
+} else {
+    console.log('invalid arg');
+}
 
